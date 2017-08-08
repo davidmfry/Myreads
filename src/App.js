9b281e2f3dfb55c4fa6441c12d6423225b4aac71
David Fry <david.fry.tv@gmail.com>
2017-08-07 20:10:09 -0700

    Search page can use the BooksAPI to search for books and add them to the diffrent shelfs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI'
 import logo from './logo.svg';
 import './App.css';
@@ -7,6 +8,7 @@ import './App.css';
 import SearchBooks from './components/SearchBooks';
 import Header from './components/Header';
 import BookShelf from './components/Bookshelf';
+import AddBook from './components/AddBook';
 
 
 
@@ -20,11 +22,26 @@ class App extends Component
         // Moves a book into the correct shelf when the users changes the shelf property
         // It finds the chosen book in the State and updates it's shelf property coming from <Book />
         let bookToChange = this.state.books.find(book => book.id === bookData.id);
-        bookToChange.shelf = bookData.newShelf;
 
-        // Triggers a rerender with the new data in state
+        if (bookToChange === undefined)
+        {
+            BooksAPI.get(bookData.id).then( (book) => {
+                book.shelf = bookData.newShelf;
+                this.setState({
+                    books: this.state.books.concat(book)
+                })
+            })
+        }
+        else
+        {
+            bookToChange.shelf = bookData.newShelf;
+        }
+
+
+        // Triggers a re-render with the new data in state
         this.setState(this.state);
     };
+
 
     componentDidMount()
     {
@@ -37,10 +54,21 @@ class App extends Component
     render() {
         return (
           <div className="App">
-              <Header/>
-              <BookShelf shelfTitle="Currently Reading" books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
-              <BookShelf shelfTitle="Want To Read" books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
-              <BookShelf shelfTitle="Read" books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
+              <Route exact path="/" render={ () => (
+                  <div className="list-books">
+                      <Header/>
+                      <BookShelf shelfTitle="Currently Reading" books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
+                      <BookShelf shelfTitle="Want To Read" books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
+                      <BookShelf shelfTitle="Read" books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
+                      <AddBook/>
+                  </div>
+              )}/>
+
+              <Route path="/search" render={ () => (
+                  <SearchBooks books={this.state.books} onShelfChange={this.handleBookShelfChange}/>
+              )}/>
+
+
           </div>
         );
     }
--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,30 +1,81 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+//API
+import * as BooksAPI from '../BooksAPI';
+
+// Components
 import Book from "./Book";
 
 class SearchBooks extends Component
 {
+    state = {
+        query: '',
+        results: [],
+        noResult: false
+    };
+
+    updateQuery(query)
+    {
+        this.setState({ query: query })
+    }
+
+    handleSubmit(event)
+    {
+        event.preventDefault();
+        BooksAPI.search(this.state.query, 20).then( (results) => {
+            if (Array.isArray(results))
+            {
+                this.setState({ noResult:false, results:results });
+            }
+            else
+                this.setState({noResult: true, query: ''});
+        })
+    }
+
     render() {
+
+        let searchResults = null;
+
+        if (this.state.noResult)
+        {
+            searchResults = <div>
+                <h3>Did not find any results. Please try again.</h3>
+            </div>
+        }
+        else
+        {
+            searchResults = <ol className="books-grid">
+                {this.state.results.map( (book, index) => (
+                    <li key={index}>
+                        <Book
+                            onShelfChange={this.props.onShelfChange}
+                            id={book.id}
+                            title={book.title}
+                            authors={book.authors}
+                            image={book.imageLinks.thumbnail}
+                        />
+                    </li>
+                ))}
+            </ol>
+        }
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search"></a>
+                    <Link to="/" className="close-search"></Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author"/>
+                        <form onSubmit={(event) => this.handleSubmit(event)}>
+                            <input type="text"
+                                   placeholder="Search by title, author or category"
+                                   value={this.state.query}
+                                   onChange={ (event) => this.updateQuery(event.target.value)}
+                            />
+                        </form>
                     </div>
                 </div>
-
                 <div className="search-books-results">
-                    <ol className="books-grid">
-                        {this.props.books.map( (book) => (
-                            <li>
-                                <Book
-                                    title={book.title}
-                                    authors={book.authors}
-                                    image={book.imageLinks.thumbnail}
-                                />
-                            </li>
-                        ))}
-                    </ol>
+                    {searchResults}
                 </div>
             </div>
         );
